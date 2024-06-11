07c12903a0340e235ce848e98a982bcfaf1a0774
ernesto <edonado@urbe.edu.ve>
2024-06-11 13:40:06 -0700

    Add project files.

---
+++ WebApplication1/Data/Migrations/20240611175301_fix_data_type.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_data_type : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "jokeQuestion",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "jokeAnswer",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "jokeQuestion",
+                table: "Joke",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "jokeAnswer",
+                table: "Joke",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ WebApplication1/Models/Joke.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class Joke
+    {
+        public Joke() { 
+        
+        }
+        public int Id { get; set; }
+        public string jokeQuestion { get; set; }
+        public string jokeAnswer { get; set; }
+    }
+}
